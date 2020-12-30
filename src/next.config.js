3bd501e02eb14d74576ae53fb2f05504788c2c0a
katoharuka <katoharuka@katoharukanoMacBook-Air.local>
2020-12-31 04:15:38 +0900

    add branch name

--- src/next.config.js
+++ src/next.config.js
@@ -1,5 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/katoharu432/katoharu432.github.io/' : '',
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/katoharu432/katoharu432.github.io/gh-pages/' : '',
 }
